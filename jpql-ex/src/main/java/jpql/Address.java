aed6bbd91a76d801801a86eefb35d5ecadd28a97
Goathoon <ownest2@gmail.com>
2024-07-13 23:47:14 +0900

    chore: setting for real-jpa-with-spring

---
+++ jpql-ex/src/main/java/jpql/Address.java
@@ -0,0 +1,34 @@
+package jpql;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ jpql-ex/src/main/java/jpql/Main.java
@@ -0,0 +1,31 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql-ex");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setUsername("태훈");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
