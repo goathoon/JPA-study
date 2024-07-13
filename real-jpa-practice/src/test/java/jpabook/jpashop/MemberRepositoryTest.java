206bd424a997f3e797ff8605d3c77838ff401a0f
Goathoon <ownest2@gmail.com>
2024-07-14 00:57:54 +0900

    test: jpa 동작 테스트 및 쿼리 로깅 라이브러리 설정

---
+++ real-jpa-practice/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+    }
+}
