b5ed4040e9879d3f0bd92775f1bdf35c6b64991f
Goathoon <ownest2@gmail.com>
2024-07-26 00:50:29 +0900

    feat: Entity 설계 완료

---
+++ real-jpa-practice/src/main/java/domain/item/Movie.java
@@ -0,0 +1,16 @@
+package domain.item;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter
+@Setter
+public class Movie extends Item{
+    private String director;
+    private String actor;
+}
